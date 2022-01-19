3351df0b3af0ee543633725221a31675081a2aad
gilmarferrini <ferrinigilmar@gmail.com>
2022-01-19 14:15:29 -0300

    feat: adicionado servico de repositorios

---
+++ app/js/services/repository.service.js
@@ -0,0 +1,7 @@
+app.factory("repositoryService", function ($http) {
+  return {
+    findRepositoriesByName: function (repositoryName, page = 1) {
+      return $http.get(`https://api.github.com/search/repositories?q=${repositoryName}&per_page=10&page=${page}`)
+    }
+  }
+})
