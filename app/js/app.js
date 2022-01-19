8e18be95b718104e4e080e4de355ac40da2777bf
gilmarferrini <ferrinigilmar@gmail.com>
2022-01-19 11:15:39 -0300

    refactor: movendo rotas para arquivo separado

---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+var app = angular.module("repositories", ['ngRoute'])
+
+app.controller("AppController", function($scope) {
+  $scope.message = 'Hello world'
+})
--- app/js/routes/index.js
+++ app/js/routes/index.js
@@ -1,9 +1,3 @@
-var app = angular.module("repositories", ['ngRoute'])
-
-app.controller("AppController", function($scope) {
-  $scope.message = 'Hello world'
-})
-
 app.config(function($locationProvider) {
   $locationProvider.html5Mode(true)
 })
