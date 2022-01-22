98fc161bed9b6845d6bdef540801217829bac10a
gilmarferrini <ferrinigilmar@gmail.com>
2022-01-22 16:13:06 -0300

    feat: adicionado logica da paginacao

--- app/js/controllers/repository.controller.js
+++ app/js/controllers/repository.controller.js
@@ -3,6 +3,7 @@ app.controller("RepositoryController", function ($scope, repositoryService) {
   $scope.repositories = []
   $scope.totalRepositories = 0
   $scope.currentPage = 1
+  $scope.maxPages = 0
 
   $scope.searchRepository = function (repositoryName, page) {
 
@@ -17,9 +18,30 @@ app.controller("RepositoryController", function ($scope, repositoryService) {
 
       $scope.totalRepositories = response.data.total_count
       $scope.repositories = response.data.items
+      $scope.maxPages = Math.ceil(response.data.total_count / 10)
+
       console.log($scope.repositories)
     })
+  }
 
+  $scope.nextPage = function () {
+    if ($scope.currentPage === $scope.maxPages) {
+      return;
+    }
 
+    $scope.currentPage += 1
+
+    $scope.searchRepository($scope.repositoryName, $scope.currentPage)
+
+  }
+
+  $scope.previousPage = function () {
+    if ($scope.currentPage === 1) {
+      return;
+    }
+
+    $scope.currentPage -= 1
+
+    $scope.searchRepository($scope.repositoryName, $scope.currentPage)
   }
 })
