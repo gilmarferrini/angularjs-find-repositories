fdb099b5dcd65b1002d226e89b4ce347eee601a0
gilmarferrini <ferrinigilmar@gmail.com>
2022-01-23 09:10:26 -0300

    feat: adicionado metodo para buscar repositorio pelo nome e nome do usuario

--- app/js/services/repository.service.js
+++ app/js/services/repository.service.js
@@ -2,6 +2,9 @@ app.factory("repositoryService", function ($http) {
   return {
     findRepositoriesByName: function (repositoryName, page = 1) {
       return $http.get(`https://api.github.com/search/repositories?q=${repositoryName}&per_page=10&page=${page}`)
+    },
+    findRepositoryByNameAndOwnerName: function (repositoryName, ownerName) {
+      return $http.get(`https://api.github.com/repos/${ownerName}/${repositoryName}`)
     }
   }
 })
